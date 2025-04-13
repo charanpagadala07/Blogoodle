058f4e035f68fa303ef584ad5b31a50363743468
charanpagadala07 <charanpagadala2004@gmail.com>
2025-04-13 22:53:31 +0530

    payload limit updated ( server.js )

--- backend/server.js
+++ backend/server.js
@@ -21,7 +21,7 @@ import notificationRouters from "./routes/notification.route.js";
 import connectDB from "./lib/db.js";
 import cookieParser from "cookie-parser";
 
-app.use(express.json());
+app.use(express.json({limit: "5mb"}));
 app.use(express.urlencoded({extended: true}));
 app.use(cookieParser());
 
